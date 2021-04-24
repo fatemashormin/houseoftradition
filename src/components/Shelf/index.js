35993ae80df2d20cb54a1633b43a64a8e1cf64db
fatemashormin <fatemashorminorni@gmail.com>
2021-04-24 00:09:11 -0500

    sfs

---
+++ src/components/Shelf/index.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { fetchProducts } from '../../services/shelf/actions';
+
+import Spinner from '../Spinner';
+import ShelfHeader from './ShelfHeader';
+import ProductList from './ProductList';
+
+import './style.scss';
+
+class Shelf extends Component {
+  static propTypes = {
+    fetchProducts: PropTypes.func.isRequired,
+    products: PropTypes.array.isRequired,
+    filters: PropTypes.array,
+    sort: PropTypes.string
+  };
+
+  state = {
+    isLoading: false
+  };
+
+  componentDidMount() {
+    this.handleFetchProducts();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { filters: nextFilters, sort: nextSort } = nextProps;
+    const { filters } = this.props;
+    if (nextFilters.length !== filters.length) {
+      this.handleFetchProducts(nextFilters, undefined);
+    }
+
+    if (nextSort !== this.props.sort) {
+      this.handleFetchProducts(undefined, nextSort);
+    }
+  }
+
+  handleFetchProducts = (
+    filters = this.props.filters,
+    sort = this.props.sort
+  ) => {
+    this.setState({ isLoading: true });
+    this.props.fetchProducts(filters, sort, () => {
+      this.setState({ isLoading: false });
+    });
+  };
+
+  render() {
+    const { products } = this.props;
+    const { isLoading } = this.state;
+
+    return (
+      <React.Fragment>
+        {isLoading && <Spinner />}
+        <div className="shelf-container">
+          <ShelfHeader productsLength={products.length} />
+          <h1>Modern  product</h1> 
+          <ProductList products={products.slice(0, 10)} />
+          <h1>Grameen product</h1> 
+          <ProductList products={products.slice(10, 20)} />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  products: state.shelf.products,
+  filters: state.filters.items,
+  sort: state.sort.type
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchProducts }
+)(Shelf);
